869563e720dd222fdde2b09f14b4fffc9a10afba
dgolferw <dodgepward@gmail.com>
2026-06-03 10:31:47 -0700

    Update rsi.py

--- strategies/rsi.py
+++ strategies/rsi.py
@@ -81,7 +81,7 @@ class RSIMeanReversion(BaseStrategy):
                 signals[sym] = ("buy", self._get_order_fraction(sym, rsi), trail)
             elif rsi < config.WEAK_OVERSOLD and has_vol and (tier in (1, 2) or sym in config.DEFENSIVE_TICKERS):
                 signals[sym] = ("buy", self._get_order_fraction(sym, rsi), trail)
-            elif regime == "bull" and 45 < rsi < 80 and tier in (1, 2) and has_vol:
+            elif regime == "bull" and 45 < rsi < 84 and tier in (1, 2) and has_vol:
                 signals[sym] = ("buy", config.ORDER_FRACTION_TIER2_NORMAL, trail)
             else:
                 signals[sym] = ("hold", config.ORDER_FRACTION, trail)
